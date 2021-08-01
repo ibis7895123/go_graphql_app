3fdd617d6a4462bc5006a5f61431a0b38b08bb9e
tokai235 <the.summer.of.spiritual@gmail.com>
2021-08-01 19:42:04 +0900

    mysqlへの接続設定を追加

--- go/server.go
+++ go/server.go
@@ -9,15 +9,43 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/ibis7895123/go_graphql_app/graph"
 	"github.com/ibis7895123/go_graphql_app/graph/generated"
+	"github.com/jinzhu/gorm"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 const defaultPort = "8080"
+const dbUser = "localuser"
+const dbPassword = "localpass"
+const dbProtocol = "tcp(mysql-graphql:3306)"
+const dbName = "localdb"
+const dbConfig = dbUser + ":" + dbPassword + "@" + dbProtocol + "/" + dbName
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
+
+	// DB接続
+	db, err := gorm.Open("mysql", dbConfig)
+
+	// DB起動エラー返す
+	if err != nil || db == nil {
+		panic(err)
+	}
+
+	// DB終了処理(エラー時はエラーを返す)
+	defer func() {
+		if db != nil {
+			if err := db.Close(); err != nil {
+				panic(err)
+			}
+		}
+	}()
+
+	// ログ出力をON
+	db.LogMode(true)
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
