c2813a1832d85b907e5f98f69dbb95c1315413fd
tokai235 <the.summer.of.spiritual@gmail.com>
2021-08-05 14:09:51 +0900

    新規ユーザー作成テスト追加

--- go/graph/schema.resolvers_test.go
+++ go/graph/schema.resolvers_test.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/ibis7895123/go_graphql_app/graph"
 	"github.com/ibis7895123/go_graphql_app/graph/generated"
+	"github.com/ibis7895123/go_graphql_app/graph/model"
 	"github.com/ibis7895123/go_graphql_app/src/models"
 	"github.com/ibis7895123/go_graphql_app/src/util"
 	"github.com/stretchr/testify/assert"
@@ -25,16 +26,41 @@ var srv = handler.NewDefaultServer(
 	),
 )
 
-func Test_正常系_全ユーザ取得(t *testing.T) {
-	expected_user_latest := models.User{
-		ID:        "72412640063b46bc8ace2baf32dc838e",
-		Name:      "バンデューラ",
-		CreatedAt: "2021-08-03 19:44:29 +0900 JST",
-		UpdatedAt: "2021-08-03 19:44:29 +0900 JST",
+func Test_正常系_ユーザ新規作成(t *testing.T) {
+	expected_user := model.NewUser{
+		Name: "テストユーザ1",
 	}
 
-	// DB終了処理
-	defer db.Close()
+	client := client.New(srv)
+	var response struct {
+		CreateUser models.User
+	}
+
+	// httpリクエスト
+	query := `
+	mutation {
+		createUser(input: {name: "` + expected_user.Name + `" }){
+			id,
+			name,
+			created_at,
+			updated_at
+		}
+	}`
+	client.Post(query, &response)
+
+	createUser := response.CreateUser
+	assert.NotEqual(t, createUser.ID, "")
+	assert.NotEqual(t, createUser.CreatedAt, "")
+	assert.NotEqual(t, createUser.UpdatedAt, "")
+}
+
+func Test_正常系_全ユーザ取得(t *testing.T) {
+	expected_user_oldest := models.User{
+		ID:        "122f1a5a728b413c982ae835cf0d84c9",
+		Name:      "太郎",
+		CreatedAt: "2021-08-01 23:52:20 +0900 JST",
+		UpdatedAt: "2021-08-01 23:53:10 +0900 JST",
+	}
 
 	client := client.New(srv)
 	var response struct {
@@ -42,8 +68,18 @@ func Test_正常系_全ユーザ取得(t *testing.T) {
 	}
 
 	// httpリクエスト
-	client.Post(`query { users { id, name, created_at, updated_at } }`, &response)
+	query := `
+	query {
+		users {
+			id,
+			name,
+			created_at,
+			updated_at
+		}
+	}`
+	client.Post(query, &response)
 
-	assert.Equal(t, len(response.Users), 8)
-	assert.Equal(t, response.Users[0], expected_user_latest)
+	users := response.Users
+	assert.Equal(t, len(users), 9)
+	assert.Equal(t, users[len(users)-1], expected_user_oldest)
 }
